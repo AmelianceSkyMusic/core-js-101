45a829da937023369206c07a0473546ab55eac33
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-25 16:09:32 +0300

    _2022-07-25_16-09

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,11 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (!(num % 5) && !(num % 3)) return 'FizzBuzz';
+  if (!(num % 3)) return 'Fizz';
+  if (!(num % 5)) return 'Buzz';
+  return num;
 }
 
 
@@ -43,8 +46,13 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n <= 1) return 1;
+  let result = n;
+  for (let i = 1; i < n; i += 1) {
+    result *= (n - i);
+  }
+  return result;
 }
 
 
@@ -60,8 +68,12 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let result = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 
@@ -80,8 +92,9 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a + b > c && b + c > a && a + c > b) return true;
+  return false;
 }
 
 
@@ -117,8 +130,24 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if ((rect2.left >= rect1.left
+    && rect2.left <= rect1.width)
+    && (rect2.top >= rect1.top
+    && rect2.top <= rect1.height)) return true;
+  if ((rect2.width + rect2.left >= rect1.left
+    && rect2.width + rect2.left <= rect1.width)
+    && (rect2.height + rect2.top >= rect1.top
+    && rect2.height + rect2.top <= rect1.height)) return true;
+  if ((rect1.left >= rect2.left
+    && rect1.left <= rect2.width)
+    && (rect1.top >= rect2.top
+    && rect1.top <= rect2.height)) return true;
+  if ((rect1.width + rect1.left >= rect2.left
+    && rect1.width + rect1.left <= rect2.width)
+    && (rect1.height + rect1.top >= rect2.top
+    && rect1.height + rect1.top <= rect2.height)) return true;
+  return false;
 }
 
 
@@ -361,6 +390,8 @@ function getCommonDirectoryPath(/* pathes */) {
  */
 function getMatrixProduct(/* m1, m2 */) {
   throw new Error('Not implemented');
+  // console.log(m1);
+  // console.log(m2);
 }
 
 
